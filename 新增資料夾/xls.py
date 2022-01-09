e2baaf7cbeb21af170434f912cf3d10d9f6842b3
sheng1115 <96768374+sheng1115@users.noreply.github.com>
2022-01-09 21:07:38 +0800

    Update xls.py

--- 新增資料夾/xls.py
+++ 新增資料夾/xls.py
@@ -1,3 +1,4 @@
+// 參考資料 (https://ithelp.ithome.com.tw/articles/10213503) (https://www.youtube.com/watch?v=FWrdKjvJUaA)
 #import excel
 ! pip install pandas
 ! pip install openpyxl
@@ -7,7 +8,7 @@ import pandas as pd
 ! pip install Flask 
 
 from flask import Flask,render_template,request
-import xlwt
+
 app=Flask(__name__)
 @app.route('/',methods=['POST'])
 
@@ -17,11 +18,11 @@ class xls:
         pass
 
     def write():
-        df['name'] = request.form['user_name']
-        df['class'] = request.form['user_class']
-        df['student_ID'] = request.form['user_id']
-        df['account_num'] = request.form['user_account']
-        df['account_password'] = request.form['user_password']
+        df['name'] = request.form['user_name']  //要從html的input抓取資料到xls裡面，但我不太確定
+        df['class'] = request.form['user_class']  //同上
+        df['student_ID'] = request.form['user_id']  //同上
+        df['account_num'] = request.form['user_account']  //同上
+        df['account_password'] = request.form['user_password']  //同上
         pass
 
     def nameCheck():
