0f2b1659be9d5a942c9eb01acd33a4581dfa48f3
sheng1115 <96768374+sheng1115@users.noreply.github.com>
2022-01-09 21:05:06 +0800

    Update xls.py

--- 新增資料夾/xls.py
+++ 新增資料夾/xls.py
@@ -1,10 +1,27 @@
 #import excel
+! pip install pandas
+! pip install openpyxl
+import pandas as pd
+! pip install xlwt
+! pip install xlrd
+! pip install Flask 
+
+from flask import Flask,render_template,request
+import xlwt
+app=Flask(__name__)
+@app.route('/',methods=['POST'])
 
 class xls:
     def read():
+        df=pd.read_excel("filename.xlsx")
         pass
 
     def write():
+        df['name'] = request.form['user_name']
+        df['class'] = request.form['user_class']
+        df['student_ID'] = request.form['user_id']
+        df['account_num'] = request.form['user_account']
+        df['account_password'] = request.form['user_password']
         pass
 
     def nameCheck():
